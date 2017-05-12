f93c2898baccb0ffd7ed4371265101d8d85c660b
DESKTOP-SK5GAMM\212 <tocyen@gmail.com.tw>
2017-05-12 12:34:46 +0800

    儲存購物車

---
+++ savecart.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+$link= @mysqli_connect(
+		'localhost',
+		'root',
+		'',
+		'phpproject');	
+mysqli_query($link,'SET NAMES utf8');
+if(isset($_SESSION["CODE"])){
+	$code=$_SESSION["CODE"];
+	$result=mysqli_query($link," SELECT * FROM produt WHERE code='$code'");
+	while ($row=mysqli_fetch_assoc($result)){
+		echo "已存至購物車";
+	}
+
+	setcookie($code."[code]",$id,time()+3600);
+	setcookie($code."[name]",$name,time()+3600);
+	setcookie($code."[price]",$price,time()+3600);
+	setcookie($code."[quantity]",$quantity,time()+3600);
+}
+  header("Location: shoppingcart.php")
+
+?>

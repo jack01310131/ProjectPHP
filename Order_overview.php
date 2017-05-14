2f1d0291f6a492710f9f805b64285f26b054cb0e
jack01310131 <jack01310131@gmail.com>
2017-05-14 17:32:02 +0800

    增加功能：完成 刪除

--- Order_overview.php
+++ Order_overview.php
@@ -1,36 +1,35 @@
-<?php
+﻿<?php
 session_start();
-
 require("sql/linksql.php");
 
-//$link= @mysqli_connect(
-//		'localhost',
-//		'root',
-//		'21427jack',
-//		'phpproject');
 mysqli_query($link,'SET NAMES utf8');
 $result=mysqli_query($link," SELECT * FROM invoice ");
 
-while ($row=mysqli_fetch_assoc($result) ){
-	$NeedPrice=0;
-
-	echo $row['Member_Code']."   ".$row['Recipient_GetTime']."   ".$row['Recipient_Address']."<br/>";
-	$ICode=$row['Code'];
-	$result2=mysqli_query($link," SELECT * FROM list WHERE invoice_Code = '$ICode' ");
-	while ($row2=mysqli_fetch_assoc($result2) ){
-		$PCode=$row2['Produce_Code'];
-		$result3=mysqli_query($link," SELECT * FROM product WHERE Code='$PCode' ");
-		while ($row3=mysqli_fetch_assoc($result3)) {
-			echo $row3['Name'];
+while ($row=mysqli_fetch_assoc($result) )
+{
+	if ($row['status']=="no")
+	{
+		$NeedPrice=0;
+		echo "第 ".$row['Code']."筆訂單";
+		echo "&nbsp&nbsp會員編號：".$row['Member_Code']."&nbsp&nbsp取餐時間：".$row['Recipient_GetTime']."&nbsp&nbsp地址：".$row['Recipient_Address'];
+		echo "&nbsp&nbsp<a href='Config/finshOrder.php?Code=$row[Code]'>完成</a>&nbsp&nbsp<a href='Config/delOrder.php?Code=$row[Code]'>刪除</a><br/>";
+		echo "餐點內容：<br/>";
+		$ICode=$row['Code'];
+		$result2=mysqli_query($link," SELECT * FROM list WHERE invoice_Code = '$ICode' ");
+		while ($row2=mysqli_fetch_assoc($result2) )
+		{
+			$PCode=$row2['Produce_Code'];
+			$result3=mysqli_query($link," SELECT * FROM product WHERE Code='$PCode' ");
+			while ($row3=mysqli_fetch_assoc($result3)) 
+			{
+				echo $row3['Name'];
+			}
+			echo "     ".$row2['Total_Amount']."    ".$row2['Remarks']."<br/>";
+			$NeedPrice+=$row2['Total_Sum'];
 		}
-		echo "     ".$row2['Total_Amount']."    ".$row2['Remarks']."<br/>";
-		$NeedPrice+=$row2['Total_Sum'];
+		echo "<br/>總金額：".$NeedPrice."<br/><br/>";
 	}
-
-
 }
 
-
-echo "<br/><br/>".$NeedPrice;
 mysqli_close($link);
 ?>
